4c3de380e4f9c438ded3e2430225951efdaf95c2
k00ume <48445418+k00ume@users.noreply.github.com>
2025-02-19 23:17:57 +0900

    検索実行までを追加

--- selenium-google/google.py
+++ selenium-google/google.py
@@ -2,19 +2,21 @@
 import time
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def edgeDriver():
     options = webdriver.EdgeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
 
-    driver = webdriver.Edge(options=options)
+    driver = webdriver.Edge(options)
     driver.set_page_load_timeout(30)
     return driver
 
 def exec(driver):
     driver.get('https://www.google.com/')
+    driver.find_element(By.ID, "APjFqb").send_keys("selenium")
+    driver.find_elements(By.NAME, "btnK")[1].click()
     print('--- executed ---')
 
 def main():
